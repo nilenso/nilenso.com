ba57635229840c333a8e6f2bb779a6379b39d017
Srihari Sriraman <srihari@c42.in>
2013-09-27 15:22:34 +0530

    moving the scroll nav js into another file

---
+++ source/javascripts/jquery.scrollnav.js
@@ -0,0 +1,19 @@
+function selectScrollNavSection(event) {
+  activeSection = $(event.target).data("section");
+  $(".scroll-nav-section[data-nav='" + activeSection + "']").addClass("selected");
+}
+
+function deSelectScrollNavSection(event) {
+  inactiveSection = $(event.target).data("section");
+  $(".scroll-nav-section[data-nav='" + inactiveSection + "']").removeClass("selected");
+}
+
+function setUpScrollNav(numberOfNavItems) {
+  var navHeight = $(".scroll-nav").height();
+  $(".scroll-nav-section").height(navHeight/numberOfNavItems);
+  $(".bordered-section")
+    .bind('enterviewport', selectScrollNavSection)
+    .bind('leaveviewport', deSelectScrollNavSection)
+    .bullseye({offsetTop: 200, offsetHeight: -500});
+  $(".scroll-nav-section").click(selectScrollNavSection);
+}
