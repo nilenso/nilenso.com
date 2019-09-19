f3fd9ce380147744c1a1e87179b16c467844e21a
udit <yudistrange@gmail.com>
2019-09-19 13:58:27 +0530

    Add carousel to people and projects
    - Uses the slick carousel https://kenwheeler.github.io/slick/

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,38 @@
 //= require_tree .
+
+$(document).ready(function() {
+  $(".carousel").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
