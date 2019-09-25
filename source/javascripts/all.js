49cf7d81991fc9fb2f525a6f4bf25d9add00a316
udit <yudistrange@gmail.com>
2019-09-25 13:59:03 +0530

    Rejig the index page for nilenso.com 2.0

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,33 +1,72 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $(".carousel").slick({
+  $(".people-carousel").slick({
     dots: true,
     infinite: true,
-    slidesToShow: 5,
+    slidesToShow: 12,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $(".client-carousel").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 6,
     slidesToScroll: 2,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 641,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          dots: false
         }
       }
       // You can unslick at a given breakpoint now by adding:
